95cc0b23e8d52657af92abdcdd7e26584a4a7f31
hamer073 <hamer073@morris.umn.edu>
2018-02-08 14:22:20 -0600

    Added html and needed javascript file for todos
    Added a todo.html file
    -Added a button in that file
    -Added redirection for that file from /todos to /todos.html
    -Added a script tag pointing to the javascript file it needs to reference
    
    Added a link to the new todo.html from the homepage
    
    #11
    #3

---
+++ src/test/java/umm3601/user/FullTodosListFromDB.java
@@ -0,0 +1,13 @@
+package umm3601.user;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
+public class FullTodosListFromDB {
+
+
+}
